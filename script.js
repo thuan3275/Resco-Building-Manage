df22b97e63d279bafbce60516d0b8df5651aaf71
thuan3275 <josephthuan3275@gmail.com>
2026-03-25 14:32:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function handleLogin() {
         sessionStorage.setItem('staffId', id);
         sessionStorage.setItem('staffName', res.name);
         sessionStorage.setItem('staffRole', res.role);
-        alert("Thành công nhập mã!");
+        //alert("Thành công nhập mã!");
         showApp();
         //location.reload(true);
     } else {
