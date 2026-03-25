bb6c285f7b7e1f3bb695d30a0ffde3ebcf18ebe8
thuan3275 <josephthuan3275@gmail.com>
2026-03-25 16:08:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxyn6romh1aT3YRDrvXRSR8wz98WTLpfHVG0Fx4icYDQ_SiZWT_4SMskBpdD1UGYr3t/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw7gqbfMQsTwY6tkU9KtzC3yEpyQwkLbYx8nC5AQ5hyrp7UPEp6YrLYe5Li5x2yviX4/exec";
 
 // --- XỬ LÝ ĐĂNG NHẬP ---
 async function handleLogin() {
