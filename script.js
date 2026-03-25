c4a9886e23d3f69debad14f168d0d58d057f13bc
thuan3275 <josephthuan3275@gmail.com>
2026-03-25 15:00:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function showApp() {
     
     // Hiển thị lời chào và nút Đăng xuất
     const showInfo = "<div class='alert alert-info d-flex justify-content-between align-items-center'><span>Chào, <b>";
-    showInfo +=${name} +"</b> ( " + ${role} +" )</span><button class='btn btn-sm btn-outline-danger' onclick='logout()'>Đăng xuất</button></div>";
+    showInfo += name +"</b> ( " + role +" )</span><button class='btn btn-sm btn-outline-danger' onclick='logout()'>Đăng xuất</button></div>";
     document.getElementById('userInfo').innerHTML = showInfo;
 
     // Nếu là Admin, hiển thị thêm nút chuyển hướng nhanh
